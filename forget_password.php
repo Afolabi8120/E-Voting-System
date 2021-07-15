71c3c1af5597b4df86d0772b237d9733e114abda
Afolabi Temidayo Timothy <46169907+Afolabi8120@users.noreply.github.com>
2021-07-15 18:54:54 +0100

    Made some changes

--- forget_password.php
+++ forget_password.php
@@ -91,8 +91,8 @@
                         $mail->isSMTP();                                            //Send using SMTP
                         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                        $mail->Username   = 'afolabi8120@gmail.com';                     //SMTP username
-                        $mail->Password   = '07069559103';                               //SMTP password
+                        $mail->Username   = 'youremail@gmail.com';                     //SMTP username
+                        $mail->Password   = 'yourpassword';                               //SMTP password
                         $mail->SMTPSecure = 'ssl';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                         $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
